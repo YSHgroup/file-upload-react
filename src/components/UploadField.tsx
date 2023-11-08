0e12d061afd1e6ec67013047f5707b581cc887f9
YSHgroup <david.dev100@outlook.com>
2023-11-08 04:03:11 -0800

    drag and drop feature added

--- src/components/UploadField.tsx
+++ src/components/UploadField.tsx
@@ -1,22 +1,34 @@
 /** @format */
 
-import React from 'react'
+import React, {useState} from 'react'
 interface UploadFieldProps {
 	files: File[]
 }
 const UploadField: React.FC<UploadFieldProps> = ({ files }) => {
-	
+	const [dragState, setDragState] = useState(false)
+	const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
+		e.preventDefault()
+		e.stopPropagation()
+		if (e.type === 'dragenter' || e.type === 'dragover') {
+			setDragState(true)
+		} else if (e.type === 'dragleave') {
+			setDragState(false)
+		}
+
+	}
 	const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault()
+		// e.stopPropagation()
 		console.log('drop-->', )
+		setDragState(false)
 		const files = e.dataTransfer.files
 		if (files) {
-			console.log(Array.from(files))
+			console.log('dropped-->', Array.from(files))
 		}
 	}
 
 	return (
-		<div className='files-field' onDrop={handleDrop} onDragOver={event => console.log('event-', event)}>
+		<div className={'files-field' + (dragState? ' drag-active': '')} onDrop={handleDrop} onDragLeave={handleDrag} onDragOver={handleDrag}>
 			{files.length? files.map((file, i) => (
 				<div key={i} className="file">
 					<span className="file-name">{file.name}</span>
